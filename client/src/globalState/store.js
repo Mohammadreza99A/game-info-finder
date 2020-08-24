7b1787ba0bacbf7841ebbb41e75b5b61a69776c6
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2020-08-24 21:38:53 +0200

    removed redux dev tools from production

--- client/src/globalState/store.js
+++ client/src/globalState/store.js
@@ -12,7 +12,9 @@ const store = createStore(
   intialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV !== 'production' &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
